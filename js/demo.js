955a8934e214dd263f890bb86646a63a03b5b027
= <=>
2025-04-14 00:02:50 +0700

    done project_đề 5

--- js/demo.js
+++
@@ -1,7 +0,0 @@
-function openModal(modalId) {
-  document.getElementById(modalId).style.display = "block";
-}
-
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
-}
